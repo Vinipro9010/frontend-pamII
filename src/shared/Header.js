cf5499eff2b763c2dae43665a8277b2755b9df67
Rodrigo Pereira <rodrigopereira.development@gmail.com>
2026-03-04 22:17:57 -0300

    update

--- src/shared/Header.js
+++ src/shared/Header.js
@@ -1,7 +1,7 @@
 export function createHeader(pageName) {
     return `
         <ion-header>
-            <ion-toolbar color="primary">
+          <ion-toolbar style="--background: #5d4037; --color: white;">
                 <ion-icon name="cafe" style="margin-left: 15px; font-size: 24px;" slot="start"></ion-icon>
                 <ion-title>Quero Café Bar - ${pageName}</ion-title>
             </ion-toolbar>
